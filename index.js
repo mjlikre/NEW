a4ef67da1ad2a6cbc496918168f5bf5fc324e172
Michael's <mjlikre@gmail.com>
2019-07-25 14:31:40 -0700

    configure server

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const cors = reuqire('cors');
+
+const app = express()
+
+
+// db setup 
+mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true, useCreateIndex: true});
+
+// middleware setup
+app.use(morgan('combined'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+//if in production, serve cliets build folder, which is created in production only 
+if (process.env.NODE_ENV === "production"){
+    app.use(express.static('client/build'))
+}
+const routes = require('./routes')
+app.use(routes);
+
+const PORT = process.end.PORT || 3001;
+app.listen(PORT, ()=> console.log(`server started on port ${PORT}` ))
+
+  
