5cef7cfc661671e30bba90c33f761bc9a5cac149
Michael's <mjlikre@gmail.com>
2019-07-26 11:47:28 -0700

    fonfigure login strategy

---
+++ config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: 'mjlikre@4399355'
+};
---
+++ services/passport.js
@@ -0,0 +1,30 @@
+const passport    = require('passport');
+const User        = require('./../models/User');
+const config      = require('./../config');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local');
+
+//create local strategy
+
+const localOptions = { usernameField: 'email' };
+
+const localLogin = new LocalStrategy(localOptions, async (email, password, done)=>{
+    try{
+        const user = await User.findOne({email});
+        if(!user){
+            return done(null, false); // done is equivalent to next, first parameter is the error, second is the user
+        }
+        user.comparePassword(password, (err, isMatch) =>{
+            if (err) return done(err)
+            if(!isMatch){
+                return done(null, false);
+            }
+            return done(null, user);
+        });
+    }catch(e){
+        done(e);
+    }
+});
+
+passport.use(localLogin);
