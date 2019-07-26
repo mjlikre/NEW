5a4ce37ce10caeab1efb540b863a745d4a05e682
Michael's <mjlikre@gmail.com>
2019-07-26 11:21:11 -0700

    create sinup route

---
+++ models/User.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
+
+const validateEmail= function (email) {
+    return validator.isEmail(email);
+}
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    lowercase: true,
+    validate: [
+        validateEmail,
+        "Please enter a valid email address"
+    ]
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+UserSchema.pre('save', async function (next){
+    const user = this;
+    try{
+        const salt = await bcrypt.genSalt();
+        console.log('salt', salt);
+        const hash = await bcrypt.hash(user.password, salt);
+        console.log('hash', hash);
+        user.password = hash;
+        next();
+    }catch(e){
+        return next(e);
+    }
+})
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
