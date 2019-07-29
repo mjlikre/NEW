426fcd4034cba335d005c28051096fa4cdd123c0
Michael's <mjlikre@gmail.com>
2019-07-29 10:48:00 -0700

    installed packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import MyComponent from './components/MyComponent';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
+    <div >
       <h1>Hello dude</h1>
-      <MyComponent />
+
     </div>
   );
 }
